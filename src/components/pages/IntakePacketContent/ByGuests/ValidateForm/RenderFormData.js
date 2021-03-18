45d178c30a26e348f8bd3018f439bb1935451ce8
joeywire <61367542+joeywire@users.noreply.github.com>
2021-03-17 20:40:47 -0500

    progress on displaying information

--- src/components/pages/IntakePacketContent/ByGuests/ValidateForm/RenderFormData.js
+++ src/components/pages/IntakePacketContent/ByGuests/ValidateForm/RenderFormData.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Typography } from 'antd';
+
+const { Title } = Typography;
 
 const RenderFormData = ({ formData, signerInfo }) => {
   const { familyInfo, familyMember } = formData;
@@ -26,10 +28,68 @@ const RenderFormData = ({ formData, signerInfo }) => {
           <li>Email: {signerInfo.email}</li>
         </ul>
       </Card>
-      <Card title="Contact Information">
+      <Card
+        title="Contact Information"
+        headStyle={{ fontSize: '1.5rem', fontWeight: 'bold' }}
+      >
+        <Title level={5}>{phone_one.name}</Title>
         <ul>
           <li>Phone Number: {phone_one.number}</li>
+          <li>
+            {phone_one.safeToLeaveMssg
+              ? 'Safe to Leave Messages'
+              : 'Do Not Leave Messages'}
+          </li>
+        </ul>
+        <Title level={5}>{phone_two.name}</Title>
+        <ul>
           <li>Phone Number: {phone_two.number}</li>
+          <li>
+            {phone_two.safeToLeaveMssg
+              ? 'Safe to Leave Messages'
+              : 'Do Not Leave Messages'}
+          </li>
+        </ul>
+      </Card>
+      <Card
+        title="Vehicle Information"
+        headStyle={{ fontSize: '1.25rem', fontWeight: 'bold' }}
+      >
+        <ul>
+          <li>Make: {vehicle.make} </li>
+          <li>Year: {vehicle.year} </li>
+          <li>Color: {vehicle.color} </li>
+          <li>Model: {vehicle.model} </li>
+          <li>License Plate: {vehicle.license_plate} </li>
+        </ul>
+      </Card>
+      <Card
+        title="Housing history"
+        headStyle={{ fontSize: '1.25rem', fontWeight: 'bold' }}
+      >
+        <ul>
+          <li>Last Permenant Address: {last_payment_address}</li>
+          <li>
+            Length of stay at last permenant residence:{' '}
+            {homeless_info.length_at_prior_location}
+          </li>
+          <li>Most recent residency: {homeless_info.prior_location}</li>
+          <li>
+            Length at current residence:{' '}
+            {homeless_info.length_at_current_residence}
+          </li>
+          <li>
+            Number of times homeless in the past two years:{' '}
+            {homeless_info.num_times_homeless}
+          </li>
+          <li>
+            Total length of homelessness in teh past two years:{' '}
+            {homeless_info.total_len_homeless}
+          </li>
+          <li>
+            Most recent date you became homeless:{' '}
+            {homeless_info.homeless_start_date}
+          </li>
         </ul>
       </Card>
     </div>
