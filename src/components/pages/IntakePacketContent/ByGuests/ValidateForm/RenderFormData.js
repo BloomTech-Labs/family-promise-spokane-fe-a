45cb6677947b33b1492217d31f4d866745b63c17
joeywire <61367542+joeywire@users.noreply.github.com>
2021-03-18 11:59:26 -0500

    refactored cards to collapsable panels

--- src/components/pages/IntakePacketContent/ByGuests/ValidateForm/RenderFormData.js
+++ src/components/pages/IntakePacketContent/ByGuests/ValidateForm/RenderFormData.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Card, Typography, Checkbox } from 'antd';
+import { Card, Typography, Collapse } from 'antd';
 
 import formData from './mockFormValues';
 
 const { Title } = Typography;
+const { Panel } = Collapse;
 
 const RenderFormData = () => {
   const { familyInfo, familyMember } = formData;
@@ -22,7 +23,7 @@ const RenderFormData = () => {
 
   return (
     <div>
-      <h3>Form Data Goes Here</h3>
+      <h2>Lets make sure all your information is correct!</h2>
       {/* <Card title="User Information">
         <ul>
           <li>First Name:{signerInfo.first_name}</li>
@@ -30,98 +31,88 @@ const RenderFormData = () => {
           <li>Email: {signerInfo.email}</li>
         </ul>
       </Card> */}
-      <Card
-        title="Contact Information"
-        headStyle={{ fontSize: '1.5rem', fontWeight: 'bold' }}
-      >
-        <Title level={5}>{phone_one.name}</Title>
-        <ul>
-          <li>Phone Number: {phone_one.number}</li>
-          <li>
-            {phone_one.safeToLeaveMssg
-              ? 'Safe to Leave Messages'
-              : 'Do Not Leave Messages'}
-          </li>
-        </ul>
-        <Title level={5}>{phone_two.name}</Title>
-        <ul>
-          <li>Phone Number: {phone_two.number}</li>
-          <li>
-            {phone_two.safeToLeaveMssg
-              ? 'Safe to Leave Messages'
-              : 'Do Not Leave Messages'}
-          </li>
-        </ul>
-        <Title level={5}>Emergency Contact</Title>
-        <ul>
-          <li>Name: {emergencyContact.name}</li>
-          <li>Phone Number: {emergencyContact.number}</li>
-        </ul>
-      </Card>
-      <Card
-        title="Vehicle Information"
-        headStyle={{ fontSize: '1.25rem', fontWeight: 'bold' }}
-      >
-        <ul>
-          <li>Make: {vehicle.make} </li>
-          <li>Year: {vehicle.year} </li>
-          <li>Color: {vehicle.color} </li>
-          <li>Model: {vehicle.model} </li>
-          <li>License Plate: {vehicle.license_plate} </li>
-        </ul>
-      </Card>
-      <Card
-        title="Housing history"
-        headStyle={{ fontSize: '1.25rem', fontWeight: 'bold' }}
-      >
-        <ul>
-          <li>Last Permenant Address: {last_payment_address}</li>
-          <li>
-            Length of stay at last permenant residence:{' '}
-            {homeless_info.length_at_prior_location}
-          </li>
-          <li>Most recent residency: {homeless_info.prior_location}</li>
-          <li>
-            Length at current residence:{' '}
-            {homeless_info.length_at_current_residence}
-          </li>
-          <li>
-            Number of times homeless in the past two years:{' '}
-            {homeless_info.num_times_homeless}
-          </li>
-          <li>
-            Total length of homelessness in teh past two years:{' '}
-            {homeless_info.total_len_homeless}
-          </li>
-          <li>
-            Most recent date you became homeless:{' '}
-            {homeless_info.homeless_start_date}
-          </li>
-        </ul>
-      </Card>
-      <Card
-        title="Benefits and Insurance"
-        headStyle={{ fontSize: '1.25rem', fontWeight: 'bold' }}
-      >
-        <Title level={5}>Government Benefits you receive:</Title>
-        <ul>
-          {gov_benefits.RRH ? <li>RRH</li> : null}
-          {gov_benefits.snap ? <li>SNAP</li> : null}
-          {gov_benefits.cps_fps ? <li>CPS/ FPS</li> : null}
-          {gov_benefits.foodstamps ? <li>Food Stamps</li> : null}
-          {gov_benefits.housing_voucher ? <li>Housing Voucher</li> : null}
-          {gov_benefits.veteran_services ? <li>veteran_services</li> : null}
-        </ul>
-        {insurance.has_insurance ? (
-          <>
-            <Title level={5}>Insurance</Title>
-            <ul>
-              <li>Members Covered under plan: {insurance.members_covered}</li>
-              <li>Insurance Type: {insurance.health_insurance_type}</li>
-            </ul>
-          </>
-        ) : null}
-      </Card>
+      <Collapse defaultActiveKey={['1']}>
+        <Panel header="Contact Information" key="1">
+          <Title level={5}>{phone_one.name}</Title>
+          <ul>
+            <li>Phone Number: {phone_one.number}</li>
+            <li>
+              {phone_one.safeToLeaveMssg
+                ? 'Safe to Leave Messages'
+                : 'Do Not Leave Messages'}
+            </li>
+          </ul>
+          <Title level={5}>{phone_two.name}</Title>
+          <ul>
+            <li>Phone Number: {phone_two.number}</li>
+            <li>
+              {phone_two.safeToLeaveMssg
+                ? 'Safe to Leave Messages'
+                : 'Do Not Leave Messages'}
+            </li>
+          </ul>
+          <Title level={5}>Emergency Contact</Title>
+          <ul>
+            <li>Name: {emergencyContact.name}</li>
+            <li>Phone Number: {emergencyContact.number}</li>
+          </ul>
+        </Panel>
+        <Panel header="Vehicle Information" key="2">
+          <ul>
+            <li>Make: {vehicle.make} </li>
+            <li>Year: {vehicle.year} </li>
+            <li>Color: {vehicle.color} </li>
+            <li>Model: {vehicle.model} </li>
+            <li>License Plate: {vehicle.license_plate} </li>
+          </ul>
+        </Panel>
+        <Panel header="Housing history" key="3">
+          <ul>
+            <li>Last Permenant Address: {last_payment_address}</li>
+            <li>
+              Length of stay at last permenant residence:{' '}
+              {homeless_info.length_at_prior_location}
+            </li>
+            <li>Most recent residency: {homeless_info.prior_location}</li>
+            <li>
+              Length at current residence:{' '}
+              {homeless_info.length_at_current_residence}
+            </li>
+            <li>
+              Number of times homeless in the past two years:{' '}
+              {homeless_info.num_times_homeless}
+            </li>
+            <li>
+              Total length of homelessness in teh past two years:{' '}
+              {homeless_info.total_len_homeless}
+            </li>
+            <li>
+              Most recent date you became homeless:{' '}
+              {homeless_info.homeless_start_date}
+            </li>
+          </ul>
+        </Panel>
+        <Panel header="Benefits and Insurance" key="4">
+          <Title level={5}>Government Benefits you receive:</Title>
+          <ul>
+            {gov_benefits.RRH ? <li>RRH</li> : null}
+            {gov_benefits.snap ? <li>SNAP</li> : null}
+            {gov_benefits.cps_fps ? <li>CPS/ FPS</li> : null}
+            {gov_benefits.foodstamps ? <li>Food Stamps</li> : null}
+            {gov_benefits.housing_voucher ? <li>Housing Voucher</li> : null}
+            {gov_benefits.veteran_services ? <li>veteran_services</li> : null}
+          </ul>
+          {insurance.has_insurance ? (
+            <>
+              <Title level={5}>Insurance</Title>
+              <ul>
+                <li>Members Covered under plan: {insurance.members_covered}</li>
+                <li>Insurance Type: {insurance.health_insurance_type}</li>
+              </ul>
+            </>
+          ) : null}
+        </Panel>
+      </Collapse>
     </div>
   );
 };
